2f5f86311398743d5b18c3c2978fb2243ffee9e7
Inna1004m <inna1004.m@gmail.com>
2023-12-10 10:52:50 +0300

    init commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const display = document.getElementById('display'),
+$btn = document.querySelectorAll('.btn');
+let arr = [];
+let accum = null;
+
+function calculat(btnValue) {
+    let value = btnValue.innerText;
+
+    if(value === "AC"){
+        arr = [];
+        display.value = ""
+
+    } else if(value === "←"){
+        arr.pop();
+        display.value = arr.join("");
+
+    } else if(value === "="){
+        display.value = eval(accum);
+        arr = [];
+    } else {
+        arr.push(value)
+        console.log('arr', arr)
+        accum = arr.join("")
+        display.value = arr.join("")
+    }
+        
+}
+
+
+$btn.forEach(item => item.addEventListener('click', (event)=>{ 
+    let btnValue = event.target;
+    calculat(btnValue)
+
+
+}))
